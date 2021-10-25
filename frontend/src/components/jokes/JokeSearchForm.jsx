c721bd1080d8c250eb8ffd6ca3a445cabf46ce8f
Justin Farnsworth <farnswj1@tcnj.edu>
2021-10-25 09:17:40 -0400

    pressing enter on search submits form

--- frontend/src/components/jokes/JokeSearchForm.jsx
+++ frontend/src/components/jokes/JokeSearchForm.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
-import { FormControl, TextField, InputAdornment, IconButton } from '@mui/material';
+import { Box, FormControl, TextField, InputAdornment, IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 
 export default class JokeSearchForm extends React.Component {
   render() {
     const { onSearchTitleChange, handleSubmit } = this.props;
     return (
-      <FormControl fullWidth variant="outlined" onSubmit={handleSubmit}>
-        <TextField
-          id="title"
-          label="Search jokes"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end" onClick={handleSubmit}>
-                <IconButton>
-                  <SearchIcon />
-                </IconButton>
-              </InputAdornment>
-            )
-          }}
-          onChange={event => onSearchTitleChange(event.target.value)}
-        />
-      </FormControl>
+      <Box component="form" onSubmit={handleSubmit}>
+        <FormControl fullWidth variant="outlined">
+          <TextField
+            id="title"
+            label="Search jokes"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end" onClick={handleSubmit}>
+                  <IconButton>
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+            onChange={event => onSearchTitleChange(event.target.value)}
+          />
+        </FormControl>
+      </Box>
     );
   }
 }
